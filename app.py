6a8dd9687428e61b3183ad90d8b86bed967023a2
Willy <willygurning14@gmail.com>
2025-07-21 09:50:44 +0700

    fix: tambah CORS biar bisa diakses ESP32

--- app.py
+++ app.py
@@ -171,6 +171,6 @@ def ambil_hasil():
 application = app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
