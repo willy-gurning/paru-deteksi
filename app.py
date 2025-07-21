4a3b9fbd8c1eee88350dde937db50e3056247b0a
Willy <willygurning14@gmail.com>
2025-07-21 15:09:28 +0700

    Push ulang proyek deteksi batuk IoT ke GitHub

--- app.py
+++ app.py
@@ -177,5 +177,5 @@ def ambil_hasil():
 application = app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
